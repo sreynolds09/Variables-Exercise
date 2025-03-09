5a89b9919349e481ca706b6f0e22e3ade922ba9b
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-03-09 15:04:15 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String myName = "Sara";
+            int myAge = 30;
+            char middleInitial = 'C';
+            double myShowsWatched = 1400.0;
+            decimal myTimeSpent = 30000.00m;
+            bool hasDog = true;
+            Console.WriteLine($"My Name is {myName}. I am {myAge} years old. My Middle Name begins with {middleInitial}.The number of shows I've watched is {myShowsWatched}.The amount of time I have spent watching shows is {myTimeSpent}.I have a dog {hasDog}");
         }
     }
 }
